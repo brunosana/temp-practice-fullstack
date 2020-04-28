f63932a20d4e8447527a85e58795991fc817d837
brunosana <brunosana@outlook.com.br>
2020-04-28 10:19:54 -0300

    update directory notes.html

--- react_udemy/notes/notes_script2.js
+++ react_udemy/notes/notes_script2.js
@@ -16,3 +16,23 @@ function deleteLocal(){
     localStorage.removeItem("name");
     alert("Removido!");
 }
+
+function brokenObj(){
+    const obj = {
+        nome: "Bruno",
+        sobrenome: "Santana",
+        idade: 22,
+        cargo: "Estudante"
+    }
+
+    let { nome } = obj;
+    console.log(nome);
+
+    let {cargo:funcao} = obj;
+    console.log(funcao);
+
+    let {cargo:trabalho, nome:name} = obj;
+
+    console.log(trabalho, name);
+}
+
